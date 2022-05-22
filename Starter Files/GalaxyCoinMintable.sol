d9a971882ba961ceeb5e19785b90208f5ae7ca49
Patrick Thornquist <psthornquist@gmail.com>
2022-05-22 11:24:17 -0700

    coin crowdsale files

--- Starter Files/GalaxyCoinMintable.sol
+++ Starter Files/GalaxyCoinMintable.sol
@@ -1,8 +1,8 @@
 /*
-ArtBux_Token Mintable
+GalaxyCoin Mintable
 */
 
-// Write the code for the import statements and ArtBux_Token contract below:
+// Write the code for the import statements and GalaxyCoin contract below:
 
 pragma solidity ^0.5.0;
 
@@ -10,12 +10,12 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract ArtBux_Token is ERC20, ERC20Detailed, ERC20Mintable {
+contract GalaxyCoin is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
         string memory name,
         string memory symbol
     )
-        ERC20Detailed("ArtBux", "BUX", 18)
+        ERC20Detailed("GalaxyCoin", "GXY", 18)
         public
     {
        
