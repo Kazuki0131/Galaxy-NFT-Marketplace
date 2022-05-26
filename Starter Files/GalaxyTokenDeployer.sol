67e6fd250232b4c5ef97cda15a2460384fa159b9
Patrick Thornquist <psthornquist@gmail.com>
2022-05-25 19:07:00 -0700

    new names

---
+++ Starter Files/GalaxyTokenDeployer.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "./GalaxyTokenLine.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+
+contract GalaxyTokenCrowdsale is Crowdsale, MintedCrowdsale {
+    constructor(
+        uint rate,
+        address payable wallet,
+        GalaxyTokenLine token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+        // constructor body can stay empty
+    }
+}
+
+contract GalaxyTokenCrowdsaleDeployer {
+    address public galaxy_token_address;
+    address public galaxy_crowdsale_address;
+
+    function CreateNFTLine
+   
+    {
+        GalaxyToken token = new GalaxyToken(name, symbol, 0);
+        galaxy_token_address = address(token);
+
+        GalaxyTokenCrowdsale arcade_crowdsale = new GalaxyTokenCrowdsale(1, wallet, token);
+        galaxy_crowdsale_address = address(galaxy_crowdsale);
+
+        token.addMinter(galaxy_crowdsale_address);
+        token.renounceMinter();
+    }
+}
+
